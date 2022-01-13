70260bbd76a167696af033572a4c9ae0b6a9ba9b
Kellor006 <richard.jbacsa@gmail.com>
2022-01-12 19:06:53 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AddressBookV2MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBookV2MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) 
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
